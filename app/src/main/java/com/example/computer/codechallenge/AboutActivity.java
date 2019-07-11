90871141b8cd6e2405e2e6f21e224751704c45ef
Segun Francis <francis4dblues@gmail.com>
2019-07-11 16:08:40 +0100

    UI modifications

--- app/src/main/java/com/example/computer/codechallenge/AboutActivity.java
+++ app/src/main/java/com/example/computer/codechallenge/AboutActivity.java
@@ -1,7 +1,12 @@
 package com.example.computer.codechallenge;
 
+import android.net.http.SslError;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.webkit.SslErrorHandler;
+import android.webkit.WebSettings;
+import android.webkit.WebView;
+import android.webkit.WebViewClient;
 
 public class AboutActivity extends AppCompatActivity {
 
@@ -9,5 +14,19 @@ public class AboutActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_about);
+
+        WebView webView = findViewById(R.id.webView);
+
+        webView.getSettings().setJavaScriptEnabled(true);
+        webView.setWebViewClient(new WebViewClient() {
+            @Override
+            public void onReceivedSslError(WebView view, SslErrorHandler handler, SslError error) {
+                handler.proceed();
+            }
+        });
+        /*webView.getSettings().supportZoom();
+        webView.getSettings().setSupportZoom(true);
+        webView.getSettings().setBuiltInZoomControls(true);*/
+        webView.loadUrl("https://andela.com/alc/");
     }
 }
--- app/src/main/java/com/example/computer/codechallenge/MainActivity.java
+++ app/src/main/java/com/example/computer/codechallenge/MainActivity.java
@@ -1,7 +1,10 @@
 package com.example.computer.codechallenge;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +12,12 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        Button aboutButton = findViewById(R.id.button_aboutALC);
+        Button profileButton = findViewById(R.id.button_profile);
+
+        aboutButton.setOnClickListener(view -> startActivity(new Intent(MainActivity.this, AboutActivity.class)));
+
+        profileButton.setOnClickListener(view -> startActivity(new Intent(MainActivity.this, ProfileActivity.class)));
     }
 }
