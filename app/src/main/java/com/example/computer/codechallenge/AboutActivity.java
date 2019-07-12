b843d814a09d683a7e390cf32cbd118961354fc4
Segun Francis <francis4dblues@gmail.com>
2019-07-12 07:42:27 +0100

    implement: Network State Check

--- app/src/main/java/com/example/computer/codechallenge/AboutActivity.java
+++ app/src/main/java/com/example/computer/codechallenge/AboutActivity.java
@@ -1,6 +1,10 @@
 package com.example.computer.codechallenge;
 
+import android.content.Context;
+import android.net.ConnectivityManager;
+import android.net.NetworkInfo;
 import android.net.http.SslError;
+import android.support.constraint.Group;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.KeyEvent;
@@ -8,21 +12,26 @@ import android.view.View;
 import android.webkit.SslErrorHandler;
 import android.webkit.WebView;
 import android.webkit.WebViewClient;
+import android.widget.Button;
 import android.widget.ProgressBar;
 
 public class AboutActivity extends AppCompatActivity {
 
     private WebView webView;
+    private ProgressBar pb;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_about);
 
+        checkNetworkState();
+
         webView = findViewById(R.id.webView);
         // Checking saved state instance
         if (savedInstanceState != null) {
             webView.restoreState(savedInstanceState);
+            pb.setVisibility(View.GONE);
         } else {
             webView.getSettings().setJavaScriptEnabled(true);
             webView.setWebViewClient(new WebViewClient() {
@@ -34,7 +43,7 @@ public class AboutActivity extends AppCompatActivity {
                 @Override
                 public void onPageFinished(WebView view, String url) {
                     super.onPageFinished(view, url);
-                    ProgressBar pb = findViewById(R.id.progress_bar);
+                    pb = findViewById(R.id.progress_bar);
                     pb.setVisibility(View.GONE);
                 }
             });
@@ -55,5 +64,23 @@ public class AboutActivity extends AppCompatActivity {
     protected void onSaveInstanceState(Bundle outState) {
         webView.saveState(outState);
         super.onSaveInstanceState(outState);
+    }
+
+    private void checkNetworkState() {
+        ConnectivityManager connectivityManager = (ConnectivityManager) getSystemService(Context.CONNECTIVITY_SERVICE);
+        NetworkInfo networkInfo = null;
+        if (connectivityManager != null) {
+            networkInfo = connectivityManager.getActiveNetworkInfo();
+        }
+        if (networkInfo == null || !networkInfo.isConnected()) {
+            Button retry = findViewById(R.id.button_retry);
+            Group noNetworkGroup = findViewById(R.id.group);
+            noNetworkGroup.setVisibility(View.VISIBLE);
+            retry.setOnClickListener(view -> {
+                recreate();
+                pb.setVisibility(View.GONE);
+            });
+            webView.setVisibility(View.GONE);
+        }
     }
 }
