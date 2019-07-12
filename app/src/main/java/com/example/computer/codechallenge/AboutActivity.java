67f21c2e3ad7f4bc17963f6c2198153ae763b975
Segun Francis <francis4dblues@gmail.com>
2019-07-12 07:08:02 +0100

    implement: save instance state

--- app/src/main/java/com/example/computer/codechallenge/AboutActivity.java
+++ app/src/main/java/com/example/computer/codechallenge/AboutActivity.java
@@ -6,36 +6,40 @@ import android.os.Bundle;
 import android.view.KeyEvent;
 import android.view.View;
 import android.webkit.SslErrorHandler;
-import android.webkit.WebSettings;
 import android.webkit.WebView;
 import android.webkit.WebViewClient;
 import android.widget.ProgressBar;
 
 public class AboutActivity extends AppCompatActivity {
 
-    private  WebView webView;
+    private WebView webView;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_about);
 
         webView = findViewById(R.id.webView);
+        // Checking saved state instance
+        if (savedInstanceState != null) {
+            webView.restoreState(savedInstanceState);
+        } else {
+            webView.getSettings().setJavaScriptEnabled(true);
+            webView.setWebViewClient(new WebViewClient() {
+                @Override
+                public void onReceivedSslError(WebView view, SslErrorHandler handler, SslError error) {
+                    handler.proceed();
+                }
 
-        webView.getSettings().setJavaScriptEnabled(true);
-        webView.setWebViewClient(new WebViewClient() {
-            @Override
-            public void onReceivedSslError(WebView view, SslErrorHandler handler, SslError error) {
-                handler.proceed();
-            }
-
-            @Override
-            public void onPageFinished(WebView view, String url) {
-                super.onPageFinished(view, url);
-                ProgressBar pb = findViewById(R.id.progress_bar);
-                pb.setVisibility(View.GONE);
-            }
-        });
-        webView.loadUrl("https://andela.com/alc/");
+                @Override
+                public void onPageFinished(WebView view, String url) {
+                    super.onPageFinished(view, url);
+                    ProgressBar pb = findViewById(R.id.progress_bar);
+                    pb.setVisibility(View.GONE);
+                }
+            });
+            webView.loadUrl("https://andela.com/alc/");
+        }
     }
 
     @Override
@@ -45,5 +49,11 @@ public class AboutActivity extends AppCompatActivity {
             return true;
         }
         return super.onKeyDown(keyCode, event);
+    }
+
+    @Override
+    protected void onSaveInstanceState(Bundle outState) {
+        webView.saveState(outState);
+        super.onSaveInstanceState(outState);
     }
 }
